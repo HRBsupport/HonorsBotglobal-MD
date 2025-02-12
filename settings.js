8db421af4ca5137573ec4c6f664ac569040df9f0
HRBsupport <wakeoneteam@gmail.com>
2025-02-12 11:04:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'HonorsTeam',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "HonorsBot MD",
+  botOwner: 'ZERO OF HONORSTEAM', // Your name
+  ownerNumber: '628388188407', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
